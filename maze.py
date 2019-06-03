cb41f0baf29690a012d02de83c4ffa07fe3d4c78
Jeroen Postema <jeroen@script.nl>
2019-06-03 17:04:44 +0200

    finished first working version

--- maze.py
+++ maze.py
@@ -10,7 +10,7 @@ maze = [
         [0,3],[2,3],[3,3],[4,3],[5,3],[9,3],
         [0,4],[6,4],[7,4],[9,4],
         [0,5],[2,5],[4,5],[5,5],[6,5],[7,5],[9,5],
-        [0,6],[2,6],[4,6],[6,6],[7,6],[9,6],
+        [0,6],[2,6],[4,6],[9,6],
         [0,7],[2,7],[4,7],[6,7],[7,7],[9,7],
         [0,8],[2,8],[7,8],
         [0,9],[1,9],[2,9],[3,9],[4,9],[5,9],[6,9],[7,9],[8,9],[9,9]
@@ -24,7 +24,7 @@ start = [1,0]
 finish = [9,8]
 
 # execution limit
-function_call_limit = 10
+function_call_limit = 100
 function_call_count = 0
 
 # function to visit all neighbouring (left, right, top, bottom) visitable positions
@@ -49,37 +49,39 @@ def visitNextPosition(pos):
         if not isWall(top):
             visitNextPosition(top)
     else:
-        print("location already visited")
+        print("location %s already visited" % top)
 
     if bottom not in already_tried:
         print("checking bottom position")
         if not isWall(bottom):
             visitNextPosition(bottom)
     else:
-        print("location already visited")
+        print("location %s already visited" % bottom)
 
     if left not in already_tried:
         if not isWall(left):
             print("checking left position")
             visitNextPosition(left)
     else:
-        print("location already visited")
+        print("location %s already visited" % left)
 
     if right not in already_tried:
         if not isWall(right):
             print("checking right position")
             visitNextPosition(right)
     else:
-        print("location already visited")
+        print("location %s already visited" % right)
 
     print("reached return condition")
     return
 
 # check if pos is a wall or a position outside the maze grid
 def isWall(pos):
-    if pos in maze or pos[0] < 0 or pos[1] < 0 or pos:
+    if pos in maze or pos[0] < 0 or pos[0] > 9 or pos[1] < 0 or pos[1] > 9:
+        print("next position %s is an inaccessable position" % pos)
         return True
     else:
+        print("next position %s is an accessable position" % pos)
         return False
 
 visitNextPosition(start)
