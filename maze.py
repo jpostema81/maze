4678614852692757bf6c3e5d2edc9591bfb21a6a
Jeroen Postema <jeroen@script.nl>
2019-06-07 12:28:14 +0200

    add beginning of dynamic maze generator

--- maze.py
+++ maze.py
@@ -15,41 +15,59 @@ def main():
     blockDim = 25
 
     window = GraphWin("My Maze", blockDim*2+mazeWidth*blockDim, blockDim*2+mazeHeight*blockDim)
-    maze = createMaze(mazeWidth, mazeHeight, True)
+    maze = createStaticMaze(mazeWidth, mazeHeight)
     drawMaze(window, maze, blockDim)
-    visitNextPosition(startPosX, startPosY, maze, window, blockDim)
+    visitNextPosition(startPosX, startPosY, maze, mazeWidth, mazeHeight, window, blockDim)
     window.getMouse() # Pause to view result
     window.close()
 
-def createMaze(mazeWidth, mazeHeight, returnStaticMaze):
-    if returnStaticMaze == True:
-        # the maze
-        # w = wall
-        # g = gate
-        # s = start
-        # f = finish
-        # v = visited
-        return [['w','s','w','w','w','w','w','w','w','w'], 
-            ['w','g','w','w','g','g','g','g','g','w'],
-            ['w','g','w','w','g','w','w','w','g','w'],
-            ['w','g','w','w','w','w','g','g','g','w'],
-            ['w','g','g','g','g','g','w','w','g','w'],
-            ['w','g','w','g','w','w','w','w','g','w'],
-            ['w','g','w','g','w','g','g','g','g','w'],
-            ['w','g','w','g','w','g','w','w','g','w'],
-            ['w','g','w','g','g','g','g','w','g','f'],
-            ['w','w','w','w','w','w','w','w','w','w']]
-    else:
-        maze = [['w' for x in range(mazeWidth)] for y in range(mazeHeight)] 
-        return maze
+#create hard-coded maze
+def createStaticMaze(mazeWidth, mazeHeight):
+    # the maze
+    # w = wall
+    # g = gate
+    # s = start
+    # f = finish
+    # v = visited
+    return [['w','s','w','w','w','w','w','w','w','w'], 
+        ['w','g','w','w','g','g','g','g','g','w'],
+        ['w','g','w','w','g','w','w','w','g','w'],
+        ['w','g','w','w','w','w','g','g','g','w'],
+        ['w','g','g','g','g','g','w','w','g','w'],
+        ['w','g','w','g','w','w','w','w','g','w'],
+        ['w','g','w','g','w','g','g','g','g','w'],
+        ['w','g','w','g','w','g','w','w','g','w'],
+        ['w','g','w','g','g','g','g','w','g','f'],
+        ['w','w','w','w','w','w','w','w','w','w']]
+
+#create dynamic generated maze
+def createDynamicMaze(mazeWidth, mazeHeight):
+    maze = [['w' for x in range(mazeWidth)] for y in range(mazeHeight)]
+
+    createNextGate(x, y, maze, mazeWidth, mazeHeight)
+
+    # for y in range(mazeHeight):
+    #     maze[y][1] = 'g'
+
+    # maze[mazeHeight-1][1] = 'f'
+    return maze
+
+def createNextGate(x, y, maze, mazeWidth, mazeHeight):
+    #random.randint(1,101)
+
+    return False
 
 def drawMaze(window, maze, blockDim):
     for y in range(len(maze)):
-        for x in range(len(maze[y])):
+        for x in range(len(maze[y])):            
             if maze[y][x] == 'w':
-                drawBlock(window, x, y, 'green', blockDim)
-            else:
+                drawBlock(window, x, y, 'black', blockDim)
+            elif maze[y][x] == 'g':
                 drawBlock(window, x, y, 'white', blockDim)
+            elif maze[y][x] == 's':
+                drawBlock(window, x, y, 'blue', blockDim)
+            elif maze[y][x] == 'f':
+                drawBlock(window, x, y, 'green', blockDim)
 
 def drawBlock(window, x, y, color, blockDim):
     xPos = (x + 1) * blockDim
@@ -59,39 +77,34 @@ def drawBlock(window, x, y, color, blockDim):
     rect.draw(window)
 
 # function to visit all neighbouring (left, right, top, bottom) visitable positions
-def visitNextPosition(x, y, maze, window, blockDim):
-    if maze[y][x] == 'f':
-        print(maze[y][x])
-        drawBlock(window, x, y, 'blue', blockDim)
-        return
-
+def visitNextPosition(x, y, maze, mazeWidth, mazeHeight, window, blockDim):
     maze[y][x] = 'v'
+
+    if maze[y][x] == 'f':
+        return
 
     drawBlock(window, x, y, 'yellow', blockDim)
     time.sleep(.3)
 
-    if isVisitable(x-1, y, maze):
-        visitNextPosition(x-1, y, maze, window, blockDim)
+    if isVisitable(x-1, y, maze, mazeWidth, mazeHeight):
+        visitNextPosition(x-1, y, maze, mazeWidth, mazeHeight, window, blockDim)
 
-    if isVisitable(x+1, y, maze):
-        visitNextPosition(x+1, y, maze, window, blockDim)
+    if isVisitable(x+1, y, maze, mazeWidth, mazeHeight):
+        visitNextPosition(x+1, y, maze, mazeWidth, mazeHeight, window, blockDim)
 
-    if isVisitable(x, y-1, maze):
-        visitNextPosition(x, y-1, maze, window, blockDim)
+    if isVisitable(x, y-1, maze, mazeWidth, mazeHeight):
+        visitNextPosition(x, y-1, maze, mazeWidth, mazeHeight, window, blockDim)
 
-    if isVisitable(x, y+1, maze):
-        visitNextPosition(x, y+1, maze, window, blockDim)
+    if isVisitable(x, y+1, maze, mazeWidth, mazeHeight):
+        visitNextPosition(x, y+1, maze, mazeWidth, mazeHeight, window, blockDim)
 
     return
 
-def isVisitable(x, y, maze):
-    try:
-        if maze[y][x] == 'v' or maze[y][x] == 'w':
-            return False
-        else:
-            return True
-    except IndexError:
+def isVisitable(x, y, maze, mazeWidth, mazeHeight):
+    if x < 0 or x > mazeWidth-1 or y < 0 or y > mazeHeight-1 or maze[y][x] == 'v' or maze[y][x] == 'w':
         return False
+    else:
+        return True
 
 main()
 
